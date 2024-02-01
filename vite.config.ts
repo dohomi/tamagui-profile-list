dd03f80d8630e57cca9140d267351640f219d8d9
Dominic Garms <djgarms@gmail.com>
2024-02-01 11:43:25 +0800

    upgrade latest version and add production profiling

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,23 @@ import { tamaguiExtractPlugin, tamaguiPlugin } from '@tamagui/vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
-const shouldExtract = process.env.EXTRACT === '1'
 
+const isProduction = process.env.NODE_ENV === 'production';
+const shouldExtract =  isProduction|| process.env.EXTRACT === '1'
+const profiling = isProduction && {
+  "react-dom/client": "react-dom/profiling"
+}
 const tamaguiConfig = {
   components: ['tamagui'],
   config: 'src/tamagui.config.ts',
 }
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      ...profiling
+    }
+  },
   clearScreen: true,
   plugins: [
     react(),
